369dd8d5903f92ddf760162ca76814e9c4447c73
MD TALHA BIN ABDULLAH <jubairt35@gmail.com>
2023-12-28 15:37:09 +0600

    csrf trusted origins

--- my_bank/settings.py
+++ my_bank/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://bank-management-django.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
